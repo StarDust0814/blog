0a54f247be40f45ec21018d5a82262e716f7b344
frank <876700669@qq.com>
2024-02-12 21:05:35 +0800

    add:添加导出excel

--- sangeng-admin/src/main/java/com/sangeng/controller/CategoryController.java
+++ sangeng-admin/src/main/java/com/sangeng/controller/CategoryController.java
@@ -1,14 +1,21 @@
 package com.sangeng.controller;
 
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.fastjson.JSON;
 import com.sangeng.domain.ResponseResult;
 import com.sangeng.domain.entity.Category;
 import com.sangeng.domain.vo.CategoryVo;
+import com.sangeng.domain.vo.ExcelCategoryVo;
+import com.sangeng.enums.AppHttpCodeEnum;
 import com.sangeng.service.CategoryService;
+import com.sangeng.utils.BeanCopyUtils;
+import com.sangeng.utils.WebUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
@@ -21,5 +28,18 @@ public class CategoryController {
     public ResponseResult listAllCategory(){
          List<CategoryVo> list = categoryService.listAllCategory();
          return ResponseResult.okResult(list);
+     }
+
+     @GetMapping("/export")
+    public void export(HttpServletResponse response){
+        try{
+            WebUtils.setDownLoadHeader("分类.xlsx",response);
+            List<Category> categoryVos = categoryService.list();
+            List<ExcelCategoryVo> excelCategoryVos = BeanCopyUtils.copyBeanList(categoryVos, ExcelCategoryVo.class);
+            EasyExcel.write(response.getOutputStream(),ExcelCategoryVo.class).autoCloseStream(Boolean.FALSE).sheet("分类导出").doWrite(excelCategoryVos);
+        }catch (Exception e){
+            ResponseResult result = ResponseResult.errorResult(AppHttpCodeEnum.SYSTEM_ERROR);
+            WebUtils.renderString(response, JSON.toJSONString(result));
+        }
      }
 }
--- sangeng-framework/src/main/java/com/sangeng/utils/WebUtils.java
+++ sangeng-framework/src/main/java/com/sangeng/utils/WebUtils.java
@@ -2,6 +2,8 @@ package com.sangeng.utils;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class WebUtils
 {
@@ -25,5 +27,15 @@ public class WebUtils
             e.printStackTrace();
         }
         return null;
+    }
+
+    /**
+     * 导出EXCEL
+     */
+    public static void setDownLoadHeader(String filename, HttpServletResponse response) throws UnsupportedEncodingException {
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        response.setCharacterEncoding("utf-8");
+        String fname = URLEncoder.encode(filename, "UTF-8").replaceAll("\\+", "%20");
+        response.setHeader("Content-disposition", "attachment; filename=''" + fname);
     }
 }
